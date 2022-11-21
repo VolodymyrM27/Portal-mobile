79ade3b2baaea757398e3cd6b8ad6c745c3a0ed8
metraakaldap <metraakladap228@gmail.com>
2022-11-21 07:08:48 +0200

    MainScreen almost

--- app/src/main/java/com/example/portal/ui/MainPage.kt
+++ app/src/main/java/com/example/portal/ui/MainPage.kt
@@ -1,22 +1,26 @@
 package com.example.portal.ui
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Lock
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.*
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.portal.CustomCircularProgressIndicator
 import com.example.portal.StyledButton
 import com.example.portal.R
 import com.example.portal.responses.UserResponse
 
-
+@Preview(showBackground = true)
 @Composable
-fun MainPage(userResponse: UserResponse?, onLogOutClick: () -> Unit, isLoading: Boolean) {
+fun MainPage(userResponse: UserResponse? = null, onLogOutClick: () -> Unit= {}, isLoading: Boolean = false) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
