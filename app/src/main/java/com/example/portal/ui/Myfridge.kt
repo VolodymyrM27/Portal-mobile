e9c4f8aef52d4c285c1985e17d2714db7d9f2a17
Яковенко Дима <dmityakovenko@gmail.com>
2022-11-28 16:17:43 +0200

    Add deleting

--- app/src/main/java/com/example/portal/ui/Myfridge.kt
+++ app/src/main/java/com/example/portal/ui/Myfridge.kt
@@ -48,6 +48,7 @@ fun MyFridge(userLogin: String){
 
         }
         FridgeItem(R.drawable.ic_launcher_background,"Смачні кадировці у власному соку",10.5)
+        FridgeItem(R.drawable.ic_launcher_background,"Смачні кадировці у власному соку",10.5)
     }
 }
 
@@ -56,47 +57,55 @@ fun FridgeItem(picture: Int, name: String, capacity: Double){
     var capacitynew = remember {
         mutableStateOf(capacity)
     }
-    Card(modifier = Modifier
-        .fillMaxWidth()
-        .padding(horizontal = 15.dp, vertical = 5.dp),
-        shape = RoundedCornerShape(15.dp),
-        LightGreen) {
-        Box(modifier = Modifier
-            .size(0.dp, 100.dp)
-            .fillMaxWidth(),
-        ){
-            Row(
-                horizontalArrangement = Arrangement.SpaceBetween,
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Image(painter = painterResource(id = picture),
-                    contentDescription = "fridgeitem",
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .padding(10.dp)
-                        .size(80.dp)
-                        .clip(CircleShape),
+    var isreal = remember {
+        mutableStateOf(true)
+    }
+    if (isreal.value){
+        Card(modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 15.dp, vertical = 5.dp),
+            shape = RoundedCornerShape(15.dp),
+            LightGreen) {
+            Box(modifier = Modifier
+                .size(0.dp, 100.dp)
+                .fillMaxWidth(),
+            ){
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Image(painter = painterResource(id = picture),
+                        contentDescription = "fridgeitem",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .padding(10.dp)
+                            .size(80.dp)
+                            .clip(CircleShape),
                     )
-                Column(
-                    modifier = Modifier.padding(vertical = 10.dp),
-                ) {
-                    Text(text = "Смачні кадировці у власному соку", modifier =  Modifier.fillMaxWidth(0.8f), maxLines = 2, style = MaterialTheme.typography.h6)
-                    Text(text = "Кількість: ${capacitynew.value} кг", modifier =  Modifier.fillMaxWidth(0.8f), style = MaterialTheme.typography.h6)
-                }
-                Column(
-                    modifier = Modifier.padding(vertical = 10.dp),
-                    verticalArrangement = Arrangement.SpaceAround
-                ) {
-                    IconButton(onClick = {
-                        capacitynew.value += 1 ;
-                    }) {
-                        Icon(Icons.Filled.Edit,null)
+                    Column(
+                        modifier = Modifier.padding(vertical = 10.dp),
+                    ) {
+                        Text(text = "Смачні кадировці у власному соку", modifier =  Modifier.fillMaxWidth(0.8f), maxLines = 2, style = MaterialTheme.typography.h6)
+                        Text(text = "Кількість: ${capacitynew.value} кг", modifier =  Modifier.fillMaxWidth(0.8f), style = MaterialTheme.typography.h6)
                     }
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(Icons.Filled.Clear,null)
+                    Column(
+                        modifier = Modifier.padding(vertical = 10.dp),
+                        verticalArrangement = Arrangement.SpaceAround
+                    ) {
+                        IconButton(onClick = {
+                            capacitynew.value += 1 ;
+                        }) {
+                            Icon(Icons.Filled.Edit,null)
+                        }
+                        IconButton(onClick = {
+                            isreal.value = false;
+                        }) {
+                            Icon(Icons.Filled.Clear,null)
+                        }
                     }
                 }
             }
-        }
+    }
+
     }
 }
